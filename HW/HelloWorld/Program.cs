3523f6533847a018c893312dfd11365c8aeb9583
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-07-22 15:18:00 -0700

    HOP01
    late submit

---
+++ HW/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "fernando";
+            int age = 25;
+            bool isMale = true;
+            float MyFloat = 10.1f;
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(isMale);
+            Console.WriteLine(MyFloat);
+        }
+    }
+}
+
