475a86ffdf6687d4c8817de1997034bae492081e
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-07-22 17:33:57 -0700

    HPO2
    Module 2

---
+++ Arrays_Enum/Program.cs
@@ -0,0 +1,43 @@
+﻿/********
+**
+** Name: Mckenzie, Fernando
+** Class: CS 132
+** Project: HOP02
+** Date: 2020-07-22
+** Description: Arrays and Enumeration.
+** Include those aspects that would be relevant for someone
+** viewing the project without any prior information about the project.
+**
+********/
+
+
+using System;
+using System.Globalization;
+
+namespace Arrays_Enum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[5];  //creating a new array of type interger named 'numbers'
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            numbers[4] = 5;
+
+            //print ou the whole array
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //
+            //}
+
+            foreach (int number in numbers)   
+            {
+                Console.Write(number + " "); 
+            }
+           
+        }
+    }
+}
