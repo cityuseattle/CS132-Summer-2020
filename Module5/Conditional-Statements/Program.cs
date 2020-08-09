5c72002cdfd296caa8c684701ba92be99d6236f4
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-08-09 04:42:41 -0700

    Module5
    Commit

---
+++ Module5/Conditional-Statements/Program.cs
@@ -0,0 +1,40 @@
+﻿/*****
+ **
+ ** Name:Mckenzie,Fernando
+ ** Class: CS132
+ ** Project:HOP05 Conditional Structures and loops
+ ** Date: 2020-08-05
+ ** Description: Implement lesson learned from chapter 5
+ **
+ *****/
+
+using System;
+using System.ComponentModel;
+using static System.Console;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 6;
+            Console.WriteLine("Guess a number (1-10):");
+            int userInput = Convert.ToInt32(ReadLine());  // receive user input and store in "userInput"  variable
+            
+            if(userInput == num)
+            {
+                WriteLine("your guess was correct");
+             
+            }
+            else if (userInput >= num)
+            {
+                WriteLine("must be 1 - 10!!!");
+            }      
+            else if (userInput != num)
+            {
+                WriteLine("you guessed wrong. Goodluck next time!");
+            }
+        }  
+    }
+}
---
+++ Module5/While-Loop/Program.cs
@@ -0,0 +1,49 @@
+﻿/*****
+ **
+ ** Name:Mckenzie,Fernando
+ ** Class: CS132
+ ** Project:HOP05 Conditional Structures and loops/ challenge
+ ** Date: 2020-08-05
+ ** Description: Implement lesson learned from chapter 5
+ **
+ *****/
+
+
+using System;
+using static System.Console;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 1;
+            WriteLine("Guess a number (1 - 10):");
+            int userInput = Convert.ToInt32(ReadLine());  //Receive user input and store in "userINput" variable
+
+            while (true)
+            {
+                if (userInput <  0 || userInput >=  10)
+                {
+                    if(userInput == num)
+                    {
+                        WriteLine("Your guess was correct!");
+                        break;
+                    }
+                    else if(userInput >= 10)
+                    {
+                        WriteLine("you guessed wrong. Guess again:");
+                        userInput = Convert.ToInt32(ReadLine());
+                    }
+                    else if (userInput < 0)
+                    {
+                        WriteLine("number must be between 1 - 10. Guess again");
+                    }
+
+
+                }
+            }
+        }
+    }
+}
