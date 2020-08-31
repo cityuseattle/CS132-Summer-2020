eba66a0d300ea8f59329cd7e785711603b6d3d76
Eugenio <eucelor@yahoo.com>
2020-08-31 14:46:09 -0700

    Submission Module1

---
+++ EugenioCesar/Module1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string Name = "Eugenio";
+            int Age = 41;
+            bool isMale = true;
+            float MyFloat = 10.1f;
+
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(isMale);
+            Console.WriteLine(MyFloat);
+
+        }
+    }
+}
