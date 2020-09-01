6d04f11231ec4e15ed444c35063c1c89113853e1
Eugenio <eucelor@yahoo.com>
2020-09-01 14:17:43 -0700

    Submission Module4

---
+++ EugenioCesar/Module4/Methods-Scopes/HOP04_ CesarEugenio _01_Methods/Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Methods
+
+/********
+**
+** Name: Cesar, Eugenio
+** Class: CS 132
+** Project: Modulus
+** Date: 2020-08-01
+** Description: This program finds the modulus 
+*  of a number divided by another
+**
+********/
+
+
+{
+    class Program
+    { 
+        //this is a method called "Mod"
+        //it function is to find the modulus of a divided by b
+        public static int Mod(int a, int b)
+        {
+            int module = a % b;
+            return module;
+        }
+
+        //this method is called "Mod"
+        //this method is called when the program is ran
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Mod(20, 13));  //method "Mod" is called here
+        }
+    }
+}
---
+++ EugenioCesar/Module4/Methods-Scopes/HOP04_ CesarEugenio _02_Scopes/Scopes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Scopes
+
+/********
+**
+** Name: Cesar, Eugenio
+** Class: CS 132
+** Project: Scopes
+** Date: 2020-08-02
+** Description: This program show us how 
+*  the method, and class scope works
+**
+********/
+
+
+{
+    class Program
+    {
+        // public variable at class scope
+        public static string Str2;
+
+        // this method is called ReturnString
+        // this method returns a string
+         static void ReturnString(string Str1) 
+        {
+          string Str2 = "Programming in C#";
+
+        Console.Write(Str1);
+        Console.WriteLine(Str2);
+        }
+
+        // this method call to ReturnString Method
+        static void Main(string[] args)
+        {
+            ReturnString("CS131 "); // call the ReturnString method
+            Console.WriteLine(Str2);
+        }
+    }
+}
