ffa7dc519c159e62c42f04e818240f919113dd90
Eugenio <eucelor@yahoo.com>
2020-09-06 02:05:49 -0700

    Submission Module6

---
+++ EugenioCesar/Module6 /String-Manipulation/HOP06_ CesarEugenio _01_String-Manipulation/String-Manipulation/Program.cs
@@ -0,0 +1,39 @@
+﻿/********
+**
+** Name: Cesar, Eugenio
+** Class: CS 132
+** Project: String Manipulation
+** Date: 2020-09-06
+** Description: Print string characters.
+**
+********/
+
+
+using System;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+
+            Console.WriteLine(Welcome[0]); //Print the character at index 0 of the string "Welcome".
+            Console.WriteLine(CourseName.Substring(CourseName.Length -1));
+
+            /* string message = Welcome + CourseName;// Concatenate strings.
+
+            string message1 = string.Concat(Welcome, CourseName); // Concat string by substitution values of variables.
+            string message2 = $"Printing the string for the second time: {Welcome} {CourseName}";
+
+            Console.WriteLine(message1); 
+
+            string message = Welcome.ToUpper() + CourseName.ToLower(); // Concatenate strings.
+            
+            string message1 = string.Concat(Welcome, CourseName); // Concatenate string using concat() built method. */
+
+        }
+    }
+}
---
+++ EugenioCesar/Module6 /String-Manipulation/HOP06_ CesarEugenio _02_String_Challenge/String/Program.cs
@@ -0,0 +1,35 @@
+﻿/********
+**
+** Name: Cesar, Eugenio
+** Class: CS 132
+** Project: Strings
+** Date: 2020-09-26
+** Description: Program that find even position in a string and print it.
+**
+********/
+
+
+using System;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            string message = Welcome + CourseName; // Concatenate two strings;
+            
+
+            for (int i = 0; i < message.Length; i++) // Loop through the text string
+            {
+                if ((i % 2) == 0) // // Check it is odd or even (Means character position).
+                {
+                    Console.WriteLine(message[i]); // Print out the character at even index. 
+                }
+            }
+        }
+
+    }
+}
