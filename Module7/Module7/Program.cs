10ac539508d8c4d6ab77ae3b15420bfb40ec276d
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-09-07 20:13:23 -0700

    Module7
    All7

---
+++ Module7/Module7/Program.cs
@@ -0,0 +1,55 @@
+﻿/********
+** Name: Fernando, Mckenzie
+
+** Class: CS 132
+
+** Project:HOP7 Date_Time
+
+** Date: 2020-08-22
+
+** Description: Date time Handling and team learn.
+
+** video games or calendars
+** Almanacs 
+
+** 
+
+********/
+using System;
+using static System.Console;
+
+
+namespace Date_Time
+{
+    class Program
+    {
+        void GetPersonalInfo() 
+        {
+            WriteLine(" get nmd?: ");
+            var nmd = Convert.ToInt32(ReadLine());
+            WriteLine("What is the current month: ");
+            int month = Convert.ToInt32(ReadLine());
+            WriteLine("Enter your name: ");
+            string name = ReadLine();
+
+            WriteLine("What year were you born?: ");
+            int birthyear = Convert.ToInt32(ReadLine());
+
+            int age = (Convert.ToInt32(DateTime.Now.Year) - birthyear);
+            
+            WriteLine("Here are what we know from your inputs: ");
+            WriteLine("your name: " + name);
+            WriteLine("Today is: " + DateTime.Now.ToShortTimeString());
+            WriteLine("your age: " + age);
+            WriteLine("what is the current month:" + month);
+            WriteLine("get nmd?: " + nmd);
+        }
+        static void Main() 
+        {
+            Program MyProgram = new Program();
+            MyProgram.GetPersonalInfo();
+        }
+
+    }
+
+}
