37044e8ec1cb6bc98886961b316907081bd20837
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-09-07 19:55:13 -0700

    Module1
    All1

---
+++ Module1/Module1/HelloWorld_redo/Program.cs
@@ -0,0 +1,45 @@
+﻿
+/*****
+ **
+ ** Name:Mckenzie,Fernando
+ ** Class: CS132
+ ** Project:HOP01 Data Types and Variables
+ ** Date: 2020-08-11 late
+ ** Description: Implement lesson learned from Module1
+ ** 1. Hello world
+ ** 2. print triangle
+ ** 3.name, age, and challenge float.
+ *****/
+
+
+using System;
+using static System.Console;
+
+namespace HelloWorld_redo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteLine("Hello World!");           
+
+            //WriteLine("    /|");
+            //WriteLine("   / |");
+            //WriteLine("  /  |");
+            //WriteLine(" /   |");
+            //WriteLine("/____|");
+
+            string Name = "Fernando";
+            int Age = 32;
+            bool isMale = true;
+            float myFloat = 10.1f;
+
+            WriteLine(Name);
+            WriteLine(Age);
+            WriteLine(isMale);
+            WriteLine(myFloat);
+            
+
+        }
+    }
+}
