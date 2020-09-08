7637fe68ebf19b143ff5c9c0ab9469a7874b215e
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-09-07 20:03:29 -0700

    Module4
    All4

---
+++ Module4/Module4/MethodCall/MethodCall/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+namespace MethodCall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float MATH_GRADE = 80.0f;
+            const float BIOLOGY_GRADE = 80.0f;
+            const float PROG_GRADE = 96.2f;
+            const string STUDENT_NAME = "G.Treele";
+            //const float = MATH_GRADE + BIOLOGY_GRADE + PROG_GRADE);
+            // call calculateAverage() to calculate the average grade.
+            float average = CalculateAverage(MATH_GRADE,BIOLOGY_GRADE,PROG_GRADE);
+            //result average = MATH_GRADE + BIOLOGY_GRADE + PROG_GRADE / 3 );
+            ShowAverage(STUDENT_NAME,average);
+            Console.ReadLine();
+
+        }
+
+        static float CalculateAverage(float mathGrade, float biologyGrade, float progGrade)
+            {
+            result average = (mathGrade + biologyGrade + progGrade) /3;
+        }
+        static void ShowAverage(string name, float average)
+            {
+            Console.WriteLine(name+ "average: "+ average);
+
+        }
+    }
+}
---
+++ Module4/Module4/RefVal/RefVal/Program.cs
@@ -0,0 +1,45 @@
+﻿/********
+
+**
+
+** Name: Mckenzie, Fernando
+
+** Class: CS 132
+
+** Project: RefVal
+
+** Date: 2020-08-03
+
+** Description: explicit passing arguments by Ref.
+
+**
+
+********/
+
+using System;
+
+namespace RefVal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float house = 200000.0f;
+            float salary = 75000.0f;
+
+            //Explicitly pass variables by reference. 
+            AdjustForInflation(ref house, ref salary);
+            Console.WriteLine("** Values After Inflation Adjustment**");
+            Console.WriteLine("Home: " + house);
+            Console.WriteLine("Income:" + salary);
+            Console.ReadLine();
+
+        }
+        // Receive arguments explicitly by reference.
+        static void AdjustForInflation(ref float home, ref float income){   
+            const float RATE = 1.03f;
+            home *= RATE;
+            income *= RATE;
+        }
+    }
+}
