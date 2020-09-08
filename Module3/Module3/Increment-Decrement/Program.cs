b874297608a1279668be6fafa0366ba1200b58cc
Sriracha5 <64803838+Sriracha5@users.noreply.github.com>
2020-09-07 20:00:27 -0700

    Module3
    All3

---
+++ Module3/Module3/Increment-Decrement/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+/********
+** Name: Fernando, Mckenzie
+
+** Class: CS 132
+
+** Project:HOP3 part2
+
+** Date: 2020-08-22
+
+** Description: Part 2 Increment_Decrement 
+
+** 
+**  
+
+** 
+
+********/
+using System;
+using static System.Console;
+namespace Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 0; a <= 10; a++) // loop to print num less than equal to 10 a++ increase by 1
+            {
+                WriteLine(a);
+            }
+            WriteLine("challenge");
+            {
+                for(int b = 15; b >= 1; b--)
+           
+                WriteLine(b);
+            }
+            
+            
+            
+           
+            
+
+           
+        }
+    }
+}
---
+++ Module3/Module3/NumericOperators/Program.cs
@@ -0,0 +1,45 @@
+﻿
+/********
+** Name: Fernando, Mckenzie
+
+** Class: CS 132
+
+** Project:HOP3 
+
+** Date: 2020-08-22
+
+** Description: Numeric Operators and Routines 
+
+** 
+**  
+
+** 
+
+********/
+using System;
+using static System.Console;
+
+namespace NumericOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 77;
+            float num3 = 5.2f;
+            // the first is sum, second subtract, third multiply, fourth divide, last modulus. 
+            float sum = num1 + num2 + num3;
+            int subtract = num2 - num1;
+            int multiply = num1 * num2;
+            float divide = num1 / num3;
+            int modulus = num2 % num1;
+            // console will output text and the num operators
+            WriteLine("Sum of 3 numbers is: " + sum);
+            WriteLine("77- 20 is: " + subtract);
+            WriteLine("20 * 77 is: " + multiply);
+            WriteLine("20 / 5.2 is: " + divide);
+            WriteLine("77 % 20 is: " + modulus);
+        }
+    }
+}
